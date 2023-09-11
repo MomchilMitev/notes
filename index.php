a774bc4cf8d5db4b0903ed308b6a9ae23068eca8
momchilmitev <momchilmitev87@gmail.com>
2023-09-11 16:20:19 +0200

    added some markup, about and contact pages

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
 
-echo '<h1>Hi Momo</h1>';
+$books = [
+    [
+        'title' => 'Title 1',
+        'author' => 'Author 1',
+        'year' => 2001,
+        'purchaseUrl' => 'example.com'
+    ],
+    [
+        'title' => 'Title 2',
+        'author' => 'Author 2',
+        'year' => 2000,
+        'purchaseUrl' => 'example.com'
+    ],
+    [
+        'title' => 'Title 3',
+        'author' => 'Author 3',
+        'year' => 2000,
+        'purchaseUrl' => 'example.com'
+    ],
+    [
+        'title' => 'Title 4',
+        'author' => 'Author 2',
+        'year' => 2000,
+        'purchaseUrl' => 'example.com'
+    ],
+];
+
+//    function filter($books, $fn) {
+//        $filteredBooks = [];
+//
+//        foreach ($books as $book) {
+//            if($fn($book)) {
+//                $filteredBooks[] = $book;
+//            }
+//        }
+//
+//        return $filteredBooks;
+//    }
+
+$filteredBooks = array_filter($books, function($book) {
+    return $book['author'] === 'Author 2';
+});
+
+require "index.view.php";
